6a1ff5f5812b7981ce449740ae24122afb8752cb
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-02-28 14:37:47 -0300

    Seed com Drizzle

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,7 +1,7 @@
-import postgres from 'postgres'
-
 import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 import { env } from '../env'
+import * as schema from './schema'
 
-const connection = postgres(env.DATABASE_URL, { max: 1 })
-const db = drizzle(connection)
+const connection = postgres(env.DATABASE_URL)
+export const db = drizzle(connection, { schema })
