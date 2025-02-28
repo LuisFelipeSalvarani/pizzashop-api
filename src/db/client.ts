6ff448ffc1d39d827e6ebe53cceb64272549ef0f
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-02-28 14:16:43 -0300

    Variáveis de ambiente

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,7 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
-const connection = postgres(
-  'postgresql://docker:docker@localhost:5432/pizzashop',
-  { max: 1 }
-)
+import { drizzle } from 'drizzle-orm/postgres-js'
+import { env } from '../env'
+
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
