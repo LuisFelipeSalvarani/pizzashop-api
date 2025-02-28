30a907dbb533a1d40b3ea143265eb756f4b87f62
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-02-28 16:24:07 -0300

    Setup da Autenticação JWT

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,22 @@
+import cookie from '@elysiajs/cookie'
+import jwt from '@elysiajs/jwt'
 import { swagger } from '@elysiajs/swagger'
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import { env } from '../env'
 import { registerRestaurant } from './routes/register-restaurants'
 import { sendAuthLink } from './routes/send-auth-link'
 
 const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restaurantId: t.Optional(t.String()),
+      }),
+    })
+  )
+  .use(cookie())
   .use(swagger())
   .use(registerRestaurant)
   .use(sendAuthLink)
