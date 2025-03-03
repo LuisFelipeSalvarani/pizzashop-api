0b67eb5e87eb930e2ef18b6e207d6f28cc71ff21
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2025-03-03 10:01:34 -0300

    Rota: Aprovar Pedido

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,5 +1,6 @@
 import { swagger } from '@elysiajs/swagger'
 import { Elysia } from 'elysia'
+import { approveOrder } from './routes/approve-order'
 import { authenticateFromLink } from './routes/authenticate-from-link'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getOrderDetails } from './routes/get-order-details'
@@ -17,6 +18,7 @@ const app = new Elysia()
   .use(getProfile)
   .use(getManagedRestaurant)
   .use(getOrderDetails)
+  .use(approveOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
